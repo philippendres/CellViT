eb1254de2f655ffd5f01b36af5d35247348e1f91
Mikhail Konov <ge93qif@mytum.de>
2024-08-21 10:16:54 +0000

    Change setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="cellvit",
     version="1.0.0",
-    packages=find_packages,
+    packages=find_packages(),
     url="",
     author="Fabian Hoerst",
     include_package_data=True,
